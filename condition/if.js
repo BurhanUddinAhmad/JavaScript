1f0d69462a1f1542a5ca52630ba3be52b97f16fe
Burhanuddin <buscbd@gmail.com>
2024-02-07 12:25:45 +0600

    Added condition folder

---
+++ condition/if.js
@@ -0,0 +1,20 @@
+let birianiPrice = 250;
+
+if(birianiPrice < 300) {
+    console.log("Mama give me some biriani");
+}
+
+/***
+* if (condition) {
+*    ececute code if condition above is true;
+* } else {
+*    execute code if condition above is false;
+*}
+***/
+
+let weight = 40;
+if(weight < 20) {
+    console.log("I will carry it by myself");
+} else {
+    console.log("I will rent a rickshaw");
+}
---
+++ condition/ifelse.js
@@ -0,0 +1,15 @@
+let salary = 20000;
+let height = 44;
+let isBCS = true;
+
+// if(salary > 20000 && height > 66) {
+//     console.log("Su..........patro");
+// } else {
+//     console.log("Onno patro khujo");
+// }
+
+if(salary > 20000 || height > 66 || isBCS == true) {
+    console.log("eso baba kobul");
+} else {
+    console.log("vaag tui mokbul");
+}
