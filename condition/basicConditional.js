535c070696cfe929bb7b8fa728ccbfd5ce47136d
Burhanuddin <buscbd@gmail.com>
2024-02-07 12:49:25 +0600

    added some files

---
+++ condition/basicConditional.js
@@ -0,0 +1,84 @@
+/**
+ * COMPARISON
+ * 1. bigger: > 
+ * 2. less: <
+ * 3. equal: ==
+ * 4. greater than or equal: >= 
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 
+ * -------------------
+ * 7. and: &&
+ * 8. or: ||
+*/
+
+/**
+ * VARIABLE ==> container
+*/
+
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = "EEE";
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+*/
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN 
+ * 4. null
+ * 5. Number 
+*/
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// ----------------------------
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(applePrice < peyaraPrice);
+
+
+
+/**
+ * ==
+ * ===
+ * 
+*/
+
+// console.log(10 == '10')
+// console.log(10 === '10')
+
+// console.log(120 === 'tomato')
+
+// ---------------------
+console.log(10 != '10')
+console.log(10 !== '10')
